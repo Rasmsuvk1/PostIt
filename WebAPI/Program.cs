5afe7a3b2cbd8bc1f4e3f704a42d6f2121715afc
Rasmsuvk1 <rasmusvk1@gmail.com>
2023-10-27 12:34:06 +0200

    Final changes for creating user

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,10 @@
+using Application.IDao;
+using Application.Logic;
+using Application.ILogic;
+using FileData;
+using FileData.DAOs;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<FileContext>();
+builder.Services.AddScoped<IUserDao, UserDao>();
+builder.Services.AddScoped<IUserLogic, UserLogic>();
 
 var app = builder.Build();
 
