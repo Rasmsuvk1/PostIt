f623dc50c6da854c09387861f71b004b993cd057
Rasmsuvk1 <rasmusvk1@gmail.com>
2023-11-05 10:23:41 +0100

    Changes in Navbar and in the index page

--- BalzorPost/Program.cs
+++ BalzorPost/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using BalzorPost.Data;
 using HttpsClients.ClientInterfaces;
 using HttpsClients.Implementations;
 
